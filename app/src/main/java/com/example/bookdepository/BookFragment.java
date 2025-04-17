97cc567848d36815f64978c2faf4fde1056e71ad
Ulia758 <uliakolosova15@gmail.com>
2025-04-17 13:01:22 +0300

    Диалоговые окна

--- app/src/main/java/com/example/bookdepository/BookFragment.java
+++ app/src/main/java/com/example/bookdepository/BookFragment.java
@@ -1,5 +1,7 @@
 package com.example.bookdepository;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,11 +15,15 @@ import android.widget.EditText;
 
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class BookFragment extends Fragment {
     private static final String ARG_BOOK_ID = "book_id";
+    private static final String DIALOG_DATE = "DialogDate";
+    private static final int REQUEST_DATE = 0;
     private Book mBook;
     private EditText mTitleField;
     private Button mDateButton;
@@ -59,8 +65,18 @@ public class BookFragment extends Fragment {
             }
         });
         mDateButton = (Button) v.findViewById(R.id.book_date);
+        updateDate();
         mDateButton.setText(mBook.getDate().toString());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment
+                        .newInstance(mBook.getDate());
+                dialog.setTargetFragment(BookFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
         mReadedCheckBox = (CheckBox) v.findViewById(R.id.book_readed);
         mReadedCheckBox.setChecked(mBook.isReaded());
         mReadedCheckBox.setOnCheckedChangeListener(new
@@ -73,5 +89,23 @@ public class BookFragment extends Fragment {
              }
       });
         return v;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date) data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE
+                    );
+            mBook.setDate(date);
+            updateDate();
+            mDateButton.setText(mBook.getDate().toString());
+        }
+    }
+    private void updateDate() {
+        mDateButton.setText(mBook.getDate().toString());
     }
 }
